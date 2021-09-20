527236f85117d20d0252c43f69b771a3049fab8a
Liiaam93 <81435487+Liiaam93@users.noreply.github.com>
2021-09-20 19:41:17 +0100

    a

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 const Teams = ({ teams }) => {
+
+  const getMoves = async () => {
+    const req = await fetch(`/api/move-data/${league}`);
+    const json = await req.json();
+    setMoves(json);
+  }
+
+  const [league, setLeague] = useState('default');
+  const [moves, setMoves] = useState([]);
+
+  function handleChange(e) {
+    setLeague(e.target.value);
+  }
+
   let pokemap = teams.slice(1).map((team, index) => (
     <React.Fragment key={index}>
       <div className="MainDiv">
@@ -14,6 +29,7 @@ const Teams = ({ teams }) => {
                 <div class="child">
                   <img src={pokemon.sprite} />
                   <p>{pokemon.pokemon}</p>
+                  {moves && moves.pokemon.pokemon.toLowerCase().Recommended_Moves[0]}
                 </div>
               </React.Fragment>
             ))}
@@ -23,6 +39,12 @@ const Teams = ({ teams }) => {
           {team[0].wins && team[0].wins + "-"}
           {team[0].wins && 3 - team[0].wins}
         </div>
+        <select value={league} onChange={handleChange}>
+          <option value='Great'></option>
+          <option value='Ultra'></option>
+          <option value='Master'></option>
+          <option value='Field'></option>
+        </select>
       </div>
     </React.Fragment>
   ));
